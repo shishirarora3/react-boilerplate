8df90754cf87a5d5ef900e022a3b47debab51a1c
Shishir Arora <shishir.arora+sf@sprinklr.com>
2016-06-02 09:41:57 +0000

    initial commit

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,7 +13,7 @@ import 'sanitize.css/sanitize.css';
 
 import Img from 'components/Img';
 import Footer from 'components/Footer';
-import Banner from './banner-metal.jpg';
+import Banner from './paytm-logo.png';
 import A from 'components/A';
 
 import styles from './styles.css';
@@ -22,7 +22,7 @@ function App(props) {
   return (
     <div className={styles.wrapper}>
       <A className={styles.logoWrapper} href="https://twitter.com/mxstbr">
-        <Img className={styles.logo} src={Banner} alt="react-boilerplate - Logo" />
+       <Img className={styles.logo} src={Banner} alt="Paytm" />
       </A>
       {props.children}
       <Footer />
